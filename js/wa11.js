35783b31d85c0d6ca165f0866e2455eb70c32bfe
Annie <anca9301@colorado.edu>
2023-11-09 10:04:49 -0700

    update wa11

--- js/wa11.js
+++ js/wa11.js
@@ -1,18 +1,58 @@
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
+const credits = document.querySelector('.credits');
 
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+let filenames = ["Owl-AndyC.jpg", "Reading-JoelM.jpg", "Cheesecake-MinkM.jpg", "Lanterns-LeonC.jpg", "Rainforest-EutahM.jpg"];
 
 /* Declaring the alternative text for each image file */
+let alts = ['Photo by Andy Chilton on Unsplash: white and brown barn owl on yellow petaled flower field',
+            'Photo by Joel Muniz on Unsplash: girl reading book',
+            'Photo by Mink Mingle on Unsplash: slice blueberry cheesecake pie on white ceramic plate',
+            'Photo by Leon Contreras on Unsplash: low angle shot of paper lanterns',
+            'Photo by Eutah Mizushima on Unsplash: river between trees under blue sky'];
+  
+  
+  
 
 /* Looping through images */
+for (let i = 0; i < filenames.length; i++) {
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+    //create imgSrc link for filename
+    let imgSrc = "/img/wa11/" + filenames[i];
+
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', imgSrc);
+    newImage.setAttribute('alt', imgSrc);
+    newImage.setAttribute('')
+    thumbBar.appendChild(newImage);
+
+    //event listener for click
+    newImage.addEventListener('click', e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+}
 
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', () => {
+    if (btn.className == 'dark') {
+        btn.className = 'light';
+        btn.textContent = 'Lighten';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+
+        btn.style.backgroundColor = 'aliceblue';
+        btn.style.color = 'black';
+    }
+    else if (btn.className == 'light') {
+        btn.className = 'dark';
+        btn.textContent = 'Darken';
+        overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+
+        btn.style.backgroundColor = 'black';
+        btn.style.color = 'aliceblue';
+    }
+});
